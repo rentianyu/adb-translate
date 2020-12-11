233875a7b341ffbdb5c43770ff01ed259332bd8e
rentianyu <rentianyu@users.noreply.github.com>
2020-12-11 07:07:02 +0000

    deploy: fc05c98bdd72f0f0146c18115ed2b3ac3c76c431

---
+++ getprop_props.js
@@ -0,0 +1,29 @@
+import projectConfig from '/pagic.config.js';
+export default {
+    config: { "root": "/", ...projectConfig },
+    'pagePath': "getprop.md",
+    'layoutPath': "_layout.tsx",
+    'outputPath': "getprop.html",
+    'title': undefined,
+    'content': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: ''
+        } }),
+    'head': null,
+    'script': React.createElement(React.Fragment, null,
+        React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
+        React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
+        React.createElement("script", { src: "/index.js", type: "module" })),
+    'contentTitle': undefined,
+    'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: ''
+        } }),
+    'toc': null,
+    'author': "rentianyu",
+    'contributors': [
+        "rentianyu"
+    ],
+    'date': "2020-12-11T07:05:39.000Z",
+    'updated': null,
+    'excerpt': "",
+    'cover': undefined
+};
