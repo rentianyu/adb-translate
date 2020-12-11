cd56867c395aa3ea6b3975fd9e3459141a5d9022
rentianyu <rentianyu@users.noreply.github.com>
2020-12-11 07:31:17 +0000

    deploy: a691a255d456434cfa0a8346d39fe946453867cf

--- pagic.config.js
+++ pagic.config.js
@@ -1,4 +1,4 @@
 export default {
     srcDir: 'docs',
-    root: '/adb/'
+    root: '/adb-translate/',
 };
--- 新建 sh - 副本 - 副本 - 副本 (2)_props.js
+++ 新建 sh - 副本 - 副本 - 副本 (2)_props.js
@@ -1,4 +1,4 @@
-import projectConfig from '/adb/pagic.config.js';
+import projectConfig from '/adb-translate/pagic.config.js';
 export default {
     config: { "root": "/", ...projectConfig },
     'pagePath': "新建 sh - 副本 - 副本 - 副本 (2).md",
@@ -12,7 +12,7 @@ export default {
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
-        React.createElement("script", { src: "/adb/index.js", type: "module" })),
+        React.createElement("script", { src: "/adb-translate/index.js", type: "module" })),
     'contentTitle': undefined,
     'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
             __html: ''
@@ -22,7 +22,7 @@ export default {
     'contributors': [
         "rentianyu"
     ],
-    'date': "2020-12-11T07:17:16.000Z",
+    'date': "2020-12-11T07:29:52.000Z",
     'updated': null,
     'excerpt': "",
     'cover': undefined
